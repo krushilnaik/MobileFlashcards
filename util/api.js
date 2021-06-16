0fe70df68af72fd08561ade80e6f65940ed6b120
Krushil Naik <krushilnaik96@gmail.com>
2021-06-16 06:16:44 -0400

    Make API calls more asynchronous

--- util/api.js
+++ util/api.js
@@ -1,50 +1,62 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { DECK_STORAGE_KEY } from './constants';
 
-export const fetchDecks = () => {
-	return AsyncStorage.getItem(DECK_STORAGE_KEY)
-		.then(data => JSON.parse(data))
-		.catch(error => {
-			console.log(error);
-		});
+export const fetchDecks = async () => {
+	try {
+		const data = await AsyncStorage.getItem(DECK_STORAGE_KEY);
+		return JSON.parse(data);
+	} catch (error) {
+		console.error(error);
+	}
 };
 
-export const fetchDeck = title => {
-	return fetchDecks().then(data => data[title]);
+/**
+ * @param {string} title
+ */
+export const fetchDeck = async title => {
+	const data = await fetchDecks();
+	return data[title];
 };
 
-export const saveDeckTitle = title => {
-	return AsyncStorage.mergeItem(
-		DECK_STORAGE_KEY,
-		JSON.stringify({
-			[title]: {
-				title,
-				questions: []
-			}
-		})
-	).catch(error => {
-		console.log(error);
+/**
+ * @param {string} title
+ */
+export const saveDeckTitle = async title => {
+	try {
+		return AsyncStorage.mergeItem(
+			DECK_STORAGE_KEY,
+			JSON.stringify({
+				[title]: {
+					title,
+					questions: []
+				}
+			})
+		);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+/**
+ * @param {string} title
+ */
+export const removeDeck = async title => {
+	const data = await fetchDecks();
+	delete data[title];
+	AsyncStorage.setItem(DECK_STORAGE_KEY, JSON.stringify(data)).catch(error => {
+		console.error(error);
 	});
 };
 
-export const removeDeck = title => {
-	return fetchDecks().then(data => {
-		delete data[title];
-		AsyncStorage.setItem(DECK_STORAGE_KEY, JSON.stringify(data)).catch(error => {
-			console.log(error);
-		});
-	});
-};
+export const addCardToDeck = async (title, card) => {
+	let decks = await fetchDecks();
+	// fetchDecks().then(data => (decks = data));
 
-export const addCardToDeck = (title, card) => {
-	let decks;
-	fetchDecks().then(data => (decks = data));
+	const deck = await fetchDeck(title);
+	deck.questions.push(card);
+	decks[title] = deck;
 
-	return fetchDeck(title).then(deck => {
-		deck.questions.push(card);
-		decks[title] = deck;
-		AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify(decks)).catch(error => {
-			console.log(error);
-		});
+	AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify(decks)).catch(error => {
+		console.error(error);
 	});
 };
